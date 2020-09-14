6fb5a07af830d5fbddc57df8ddc535232800ec43
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-14 01:37:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // write your code below!
 var name = "Susan"
 var height = 35
-var message = `${na is ${height} incme}hes tall`
-console.log
+var message = `${na is ${height} incme}hes tall`sole.log
 module.exports = {name, height, message}
